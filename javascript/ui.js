8f12b8874188ca6d6050f932c2d1f093e6100930
Desirée Gunnarsson <dezzie@live.se>
2026-05-01 09:56:19 +0200

    updated ui.js - small fixes

--- javascript/ui.js
+++ javascript/ui.js
@@ -15,7 +15,7 @@ function renderCards(meals, containerId) {
         card.className = "recipe-card";
         card.innerHTML = `
         <img src="${meal.strMealThumb}" 
-        alt="${meal.strMeal}" class="card-image">
+        alt="${meal.strMeal}" class="card-img">
         <div class="card-content">
         <h3>${meal.strMeal}</h3>
         </div>
@@ -25,36 +25,7 @@ function renderCards(meals, containerId) {
 }
 
 
-function showError(message) {
-    const container = document.getElementById("card-container-summer");
+function showError(message, containerId) {
+    const container = document.getElementById(containerId);
     container.innerHTML = `<p class="error-message">${message}</p>`;
 }
-
-
-
-// TESTDATA — tas bort när api.js är klar
-const testData = [
-    {
-        strMeal: "Bruschetta",
-        strMealThumb: "https://www.themealdb.com/images/media/meals/uuuspp1511297945.jpg"
-    },
-    {
-        strMeal: "Poutine",
-        strMealThumb: "https://www.themealdb.com/images/media/meals/uuyrrx1487327597.jpg"
-    },
-    {
-        strMeal: "Smoked Haddock Kedgeree",
-        strMealThumb: "https://www.themealdb.com/images/media/meals/1550441882.jpg"
-    },
-    {
-        strMeal: "Timbits",
-        strMealThumb: "https://www.themealdb.com/images/media/meals/txsupu1511815755.jpg"
-    },
-    {
-        strMeal: "Wontons",
-        strMealThumb: "https://www.themealdb.com/images/media/meals/1525876468.jpg"
-    }
-];
-// testa visa 5 kort i varje sektion
-renderCards(testData, "card-container-summer");
-renderCards(testData, "card-container-quick");
