27e925473a1f51885c5faef22ba94986dff603b3
snwrn <kretiv.ksnwrn@gmail.com>
2026-05-02 02:50:48 +0200

    on index three recepies are loaded from db

---
+++ js/index.js
@@ -0,0 +1,60 @@
+// Hämta 3 förbestämda recept:
+// 1 dessert, 1 starter, 1 pasta
+
+Promise.all([
+    fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert")
+        .then(response => response.json()),
+
+    fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Starter")
+        .then(response => response.json()),
+
+    fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Pasta")
+        .then(response => response.json())
+])
+
+.then(([dessertData, starterData, pastaData]) => {
+
+    // Förbestämda recept-ID:n
+    const dessertId = "52893";
+    const starterId = "52840";
+    const pastaId = "52844";
+
+    // Hitta recepten
+    const dessertMeal = dessertData.meals.find(meal =>
+        meal.idMeal === dessertId
+    );
+
+    const starterMeal = starterData.meals.find(meal =>
+        meal.idMeal === starterId
+    );
+
+    const pastaMeal = pastaData.meals.find(meal =>
+        meal.idMeal === pastaId
+    );
+
+    // Lägg alla i samma array
+    const favoriteMeals = [
+        dessertMeal,
+        starterMeal,
+        pastaMeal
+    ];
+
+    // Visa recepten
+    renderCards(
+        favoriteMeals,
+        "favorites-container",
+        "Featured recipe of the week."
+    );
+
+})
+
+.catch(error => {
+
+    console.log(error);
+
+    showError(
+        "Could not load favorite recipes.",
+        "favorite-container"
+    );
+
+});
