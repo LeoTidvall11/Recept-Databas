ded557176c888b0e6f3bf3624800a7cafa39baf7
SaliSammoArtin <Salisammo@live.se>
2026-05-04 20:19:28 +0200

    restore correct code from fetchMeals refactor

--- js/desserts.js
+++ js/desserts.js
@@ -1,9 +1,60 @@
-// Funktionen hämtar efterrätter från API:et och visar dem på sidan
+// Hämtar efterrätter och visar dem på sidan
 async function init() {
-    //Väntar på att fetchMeals hämtar efterrätter från API:et
-    const meals = await fetchMeals ("Dessert")
-    //Skickar måltiderna till renderCards funktionen som bygger korten på sidan
-    renderCards(meals, "desserts")
+    try {
+        // Funktionen väntar med att hämta en lista med efterrätter
+        const meals = await fetchMeals("Dessert")
+
+        // Förbestämda dessert-ID:n
+        const summerDessertsIds = [
+            "52889",
+            "53005",
+            "53333",
+            "52891",
+            "53082"
+        ];
+
+        const quickDessertsIds = [
+            "52854",
+            "52855",
+            "52958",
+            "52966",
+            "52924"
+        ];
+
+        // Filtrera fram exakt de recepten vi vill visa
+        const summerMeals = meals.filter(meal =>
+            summerDessertsIds.includes(meal.idMeal)
+        );
+
+        const quickMeals = meals.filter(meal =>
+            quickDessertsIds.includes(meal.idMeal)
+        );
+
+        // Rendera kort med ui.js
+        renderCards(
+            summerMeals,
+            "summer-container",
+            "Perfect summer desserts."
+        );
+
+        renderCards(
+            quickMeals,
+            "quick-container",
+            "Quick and easy desserts."
+        );
+
+    } catch (error) {
+        showError(
+            "Could not load summer desserts.",
+            "summer-container"
+        );
+
+        showError(
+            "Could not load quick desserts.",
+            "quick-container"
+        );
+
+    }
 }
-//Anropar init så att allt körs
+
 init()
